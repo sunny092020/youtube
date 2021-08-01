a19dc37efb47827525ece26ae5c32edcb46b65d9
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-08-01 22:49:14 +0700

    add more apis

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -2,3 +2,36 @@ const router = require("express").Router();
 const Message = require("../models/Message");
 
 module.exports = router;
+
+// add
+
+/*
+{
+    "conversationId":"<ID>",
+    "sender":"605b3dc77a3cf74918165c06",
+    "text":"hey john, im jane"
+}
+ */
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
+  try{
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  }catch(err){
+      res.status(500).json(err)
+  }
+});
+
+// get
+router.get("/", async (req, res) => {
+  try{
+    const messages = await Message.find({
+      conversationId: req.params.conversationId
+    });
+    res.status(200).json(messages)
+  }catch(err){
+      res.status(500).json(err)
+  }
+});
+
+module.exports = router;
