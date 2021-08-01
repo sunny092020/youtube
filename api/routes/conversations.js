0b632f4b513c233f5a34ac877a17670ddd3916a6
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-08-01 22:09:34 +0700

    add conversation api

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -2,6 +2,13 @@ const router = require("express").Router();
 const Conversation = require("../models/Conversation");
 
 // new conversation
+/*
+    for testing
+    {
+        "senderId":"605b3dc77a3cf74918165c06",
+        "receiverId":"605b3dc777a3cf74918165c05"
+    }
+*/
 router.post("/", async (req, res) => {
     const newConversation = new Conversation({
         members: [req.body.senderId, req.body.receiverId]
