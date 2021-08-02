d9bdebb93b9d03df8c3d78fe110266e4d98534dc
Vu Hoang Giang <vhg.anrakutei@gmail.com>
2021-08-02 19:32:25 +0700

    add ajax call

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -2,9 +2,28 @@ import ChatOnline from "../../components/chatOnline/ChatOnline"
 import Conversation from "../../components/conversations/Conversation"
 import Message from "../../components/message/Message"
 import Topbar from "../../components/topbar/Topbar"
+import { useContext, useState, useEffect } from "react"
+import { AuthContext } from "../../context/AuthContext"
 import "./messenger.css"
+import axios from "axios"
 
 export default function Messenger() {
+    const [conversations, setConversations] = useState([]);
+    const {user} = useContext(AuthContext);
+
+    useEffect(() => {
+        const getConversations = async () => {
+            try {
+                const res = await axios.get("/conversations/" + user._id);
+                console.log(res);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        getConversations();
+    }, [user._id]);
+
     return (
         <>
             <Topbar/>
